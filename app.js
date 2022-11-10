729baab87d6a51ced0b1f7765a9b6bf646b0386c
Jessica Mosby <jessicamosby@mac.com>
2022-11-10 17:51:34 -0500

    created display functions and show beaniebabies on load

--- app.js
+++ app.js
@@ -11,7 +11,23 @@ let beanieBabies = [];
 let astroSigns = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    findBeanieBabies();
+});
+
+async function findBeanieBabies() {
+    const response = await getBeanieBabies();
+    beanieBabies = response.data;
+    displayBeanieBabies();
+}
 
 /* Display Functions */
+function displayBeanieBabies() {
+    beanieList.innerHTML = '';
+    for (let beanieBaby of beanieBabies) {
+        const beanieBabyEl = renderBeanieBabies(beanieBaby);
+        beanieList.append(beanieBabyEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
