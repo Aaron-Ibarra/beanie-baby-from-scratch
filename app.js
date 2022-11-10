a021ad92b02b09111ef85cd8160d7269604eb409
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-10 14:34:27 -0800

    got imports and DOM elements and State

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /* Imports */
-
+import { renderBeanieBabies, renderAstrologySigns } from './render.util.js';
+import { getBeanieBabies, getAstroSigns } from './fetch.utils.js';
 /* Get DOM Elements */
+const beanieForm = document.getElementById('beanie-form');
+const astroSelect = document.getElementById('astro-select');
+const beanieList = document.getElementById('beanie-babies');
 
 /* State */
+let beanieBabies = [];
+let astroSigns = [];
 
 /* Events */
 
