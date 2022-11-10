218e1466de8d195941c77178c959a33f9bde3011
Jessica Mosby <jessicamosby@mac.com>
2022-11-10 15:23:26 -0500

    added query in fetch.utils

--- fetch.utils.js
+++ fetch.utils.js
@@ -3,6 +3,7 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+let query = client.from('beanie_babies').select('*').order('title').limit(100);
 
 // export async functions that fetch data
 export async function getBeanieBabies() {
