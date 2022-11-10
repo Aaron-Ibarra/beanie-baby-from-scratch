9fb580d3173265deb8be137ecf75d24f813eb36f
Monika <moncobra@comcast.net>
2022-11-10 13:05:33 -0800

    render beanie baby function completed

--- render.util.js
+++ render.util.js
@@ -1,6 +1,40 @@
 export function renderBeanieBabies(beanieBaby) {
     const beaniebabyEl = document.createElement('li');
     beaniebabyEl.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = beanieBaby.image;
+    img.alt = beanieBaby.title;
+
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = beanieBaby.title;
+
+    const attributes = document.createElement('p');
+    attributes.classList.add('attributes');
+
+    const animal = document.createElement('span');
+    animal.textContent = beanieBaby.animal;
+
+    const subtheme = document.createElement('span');
+    subtheme.textContent = beanieBaby.subtheme;
+
+    const astroSign = document.createElement('span');
+    astroSign.textContent = beanieBaby.astroSign;
+
+    attributes.append(animal, subtheme, astroSign);
+
+    const released = document.createElement('p');
+    released.classList.add('released');
+    released.textContent = `Released ${beanieBaby.releaseYear}`;
+
+    content.append(h2, attributes, released);
+
+    beaniebabyEl.append(img, content);
+
+    return beaniebabyEl;
 }
 
 export function renderAstrologySigns() {}
